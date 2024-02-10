b272d382084ec68ecd92313917cb41d065cd6e77
BhavikOstwal <bhavikostwal45@gmail.com>
2024-02-10 19:30:52 +0530

    Modified graphs by adding axis lines and modified q.4 a bit

--- Assignment 0/Q.4.py
+++ Assignment 0/Q.4.py
@@ -8,12 +8,10 @@ steps = 0.01
 x_inp = np.arange(0,4.01,steps)
 # print(x_inp)
 
-plt.rcParams["text.usetex"] = False #set to False so that matplotlib will use its own rendering for Latex (as Latex isn't installed on my system)
-
 # print(plt.style.available)
 plt.style.use('dark_background')
-plt.plot(x_inp, f(x_inp), color = 'green')
-plt.plot(x_inp,np.zeros((401,)), color = 'purple') # showing the x-axis to separate 1st n 4th quadrant
+plt.plot(x_inp, f(x_inp), color = 'red', linewidth = 2)
+plt.plot(x_inp,np.zeros((401,)), color = 'white', linewidth = 1.5) # showing the x-axis to separate 1st n 4th quadrant
 plt.xlim(0,4)
 plt.grid()
 plt.xlabel('x', fontsize = 15)
